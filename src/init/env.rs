97040513c5b08593257e266514dc46c88f0d2506
rippor <ripple12315@163.com>
2026-06-24 19:35:55 +0800

    暂存

--- src/init/env.rs
+++ src/init/env.rs
@@ -1,3 +1,3 @@
 pub const TEST_PROFILE_ENV: &str = r###"TEST_PROFILE=ltp-safe"###;
 pub const LTP_TIMEOUT_ENV: &str = r###"LTP_TIMEOUT=45"###;
-pub const LTP_CASE_LIST_ENV: &str = r###"LTP_CASE_LIST=inotify_init1_01 inotify_init1_02"###;
+pub const LTP_CASE_LIST_ENV: &str = r###"LTP_CASE_LIST=accept02 bind02 gethostbyname_r01 recvmmsg01 send02 sendmmsg01 sendmmsg02 setsockopt04 sockioctl01"###;
